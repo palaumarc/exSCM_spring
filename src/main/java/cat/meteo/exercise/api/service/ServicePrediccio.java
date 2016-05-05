4324023308724ae46ff04f5c018fc1fbba3ae3b9
palaumarc <marcpalau91@gmail.com>
2016-05-05 12:54:54 +0200

    Add some logs

--- src/main/java/cat/meteo/exercise/api/service/ServicePrediccio.java
+++ src/main/java/cat/meteo/exercise/api/service/ServicePrediccio.java
@@ -11,6 +11,8 @@ import cat.meteo.exercise.api.model.dades.PrediccioMunicipi;
 import cat.meteo.exercise.api.model.metadades.Municipi;
 import java.io.FileNotFoundException;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -28,6 +30,8 @@ public class ServicePrediccio {
     @Autowired
     private DaoMetadades daoMetadades;
     
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+    
     public PrediccioMunicipi getPrediccio(int codiMunicipi) throws FileNotFoundException, Exception {
         List<PrediccioMunicipi> prediccions = null;
         PrediccioMunicipi selectedPred = null;
@@ -42,14 +46,16 @@ public class ServicePrediccio {
         }
         
         if (selectedPred == null) {
+            log.error("Error seleccionant predicció de municipi: codi no vàlid");
             throw new IllegalArgumentException("Codi de municipi no vàlid");
         }
         
+        log.debug("Predicció del municipi amb codi " + codiMunicipi + " seleccionada");
         return selectedPred;
     }
     
     public List<Municipi> getMetadataMunicipis() throws FileNotFoundException {
-        
+        log.debug("Metadades de tots els municipis obtinguda correctament");
         return daoMetadades.getAllMetadades();
     }
 }
--- src/main/java/cat/meteo/exercise/commons/exception/GlobalExceptionController.java
+++ src/main/java/cat/meteo/exercise/commons/exception/GlobalExceptionController.java
@@ -6,6 +6,8 @@
 package cat.meteo.exercise.commons.exception;
 
 import java.io.FileNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -18,12 +20,15 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  */
 
 @ControllerAdvice
-public class ExceptionControllerAdvice {
+public class GlobalExceptionController {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
     
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseBody
     public HttpStatusMessage exception(Exception e) {
+        log.error("Excepció: " + e);
         return new HttpStatusMessage(HttpStatus.BAD_REQUEST.toString(), 
                                      e.getClass().getSimpleName(), 
                                      e.getMessage());
@@ -33,6 +38,7 @@ public class ExceptionControllerAdvice {
     @ExceptionHandler(FileNotFoundException.class)
     @ResponseBody
     public HttpStatusMessage fileNotFoundException(Exception e) {
+        log.error("Excepció: " + e);
         return new HttpStatusMessage(HttpStatus.INTERNAL_SERVER_ERROR.toString(), 
                                      e.getClass().getSimpleName(), 
                                      e.getMessage());
