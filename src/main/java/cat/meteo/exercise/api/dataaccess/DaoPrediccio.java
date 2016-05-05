cd472a1538e0bfc3ed8e3836395a85d44733071e
palaumarc <marcpalau91@gmail.com>
2016-05-05 15:57:47 +0200

    Simplified json parsing in DAOs

--- src/main/java/cat/meteo/exercise/api/dataaccess/DaoPrediccio.java
+++ src/main/java/cat/meteo/exercise/api/dataaccess/DaoPrediccio.java
@@ -6,19 +6,12 @@
 package cat.meteo.exercise.api.dataaccess;
 
 import cat.meteo.exercise.api.model.dades.PrediccioMunicipi;
-import cat.meteo.exercise.api.model.metadades.Municipi;
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
-import com.google.gson.stream.JsonReader;
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -40,7 +33,7 @@ public class DaoPrediccio {
     
     public List<PrediccioMunicipi> getAllPrediccions() throws FileNotFoundException {
         
-        InputStream inputStream = DaoMetadades.class.getClassLoader().getResourceAsStream(prediccions_municipals);
+        InputStream inputStream = DaoPrediccio.class.getClassLoader().getResourceAsStream(prediccions_municipals);
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
 
         List<PrediccioMunicipi> prediccions = gson.fromJson(br, new TypeToken<List<PrediccioMunicipi>>(){}.getType());
